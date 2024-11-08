d246c10fecc8d961d41d2669fb8f3f06e36ee5b0
Davi Rodrigues do Nascimento <estagiario.nti02@faesa.br>
2024-11-08 14:04:55 -0300

    feat: add Student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import java.time.LocalDate;
 import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -15,7 +19,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World!");
+	public List<Student> hello() {
+		return List.of(
+			new Student(
+				1L,
+				"Davi",
+				19,
+				LocalDate.of(2005, 1, 23),
+				"davi@gmail.com"
+			)
+		);
 	}
 }
