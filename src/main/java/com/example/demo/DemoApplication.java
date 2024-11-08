af6b6140ec2e9bbc9331497a82cda79ec66608dd
Davi Rodrigues do Nascimento <estagiario.nti02@faesa.br>
2024-11-08 14:11:36 -0300

    feat: add Student Controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,33 +1,11 @@
 package com.example.demo;
-
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.student.Student;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-			new Student(
-				1L,
-				"Davi",
-				19,
-				LocalDate.of(2005, 1, 23),
-				"davi@gmail.com"
-			)
-		);
 	}
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,27 @@
 package com.example.demo.student;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
-    
+
+    @GetMapping
+	public List<Student> getStudents() {
+		return List.of(
+			new Student(
+				1L,
+				"Davi",
+				19,
+				LocalDate.of(2005, 1, 23),
+				"davi@gmail.com"
+			)
+		);
+	}
+
 }
